d4dd773ce43fb9bc579ae3731772ef8ada58f788
umbertofaenza <umberto.faenza@gmail.com>
2023-09-06 15:24:49 +0200

    feat: request 10 random emails + print them in ul

--- js/app.js
+++ js/app.js
@@ -2,6 +2,25 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      randomEmails: [],
+    };
+  },
+
+  methods: {
+    getRandomEmails() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            const responseEmail = response.data.response;
+            this.randomEmails.push(responseEmail);
+          });
+      }
+    },
+  },
+
+  created() {
+    this.getRandomEmails();
   },
 }).mount("#app");
